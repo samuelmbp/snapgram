f7effd08be3ceb087c7c218b66a181069c32bc34
Samuel Raducan <samuel.thembp@gmail.com>
2023-11-09 16:12:51 +0000

    Add message to sign in if user has an account

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -14,6 +14,7 @@ import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
 import Loader from "@/components/shared/Loader";
+import { Link } from "react-router-dom";
 
 const SignupForm = () => {
   const isLoading = false;
@@ -41,7 +42,7 @@ const SignupForm = () => {
           Create a new account
         </h2>
         <p className="text-light-3 small-medium sm:base-regular">
-          To use Snapgram enter your details
+          To use Snapgram, please enter your details
         </p>
 
         <form
@@ -110,6 +111,15 @@ const SignupForm = () => {
               "Sign Up"
             )}
           </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account?{" "}
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-1"
+            >
+              Log in
+            </Link>
+          </p>
         </form>
       </div>
     </Form>
